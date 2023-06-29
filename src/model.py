2ca7f66913ad61f11bb3d99368bde20323c1396e
Slava Chalnev <slava.chalnev@gmail.com>
2023-06-29 16:25:58 +1000

    Add model

---
+++ src/model.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+
+import torch.nn.functional as F
+
+
+class Model(nn.Module):
+    def __init__(self, m=2, n=10000):
+        super(Model, self).__init__()
+
+        # W is m x n
+        self.W = nn.Parameter(torch.Tensor(m, n))
+
+        # xavier init
+        nn.init.xavier_uniform_(self.W)
+
+        # b shape is (m,)
+        self.b = nn.Parameter(torch.zeros(m))
+    
+
+    def forward(self, x, return_h=False):
+        x = self.W @ x
+
+        if return_h:
+            return x
+        
+        x = self.W.T @ x + self.b
+
+        x = F.relu(x)
+        return x
+        
