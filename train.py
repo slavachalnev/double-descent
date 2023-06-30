ce708bc4bbd6a26198ed7cad2c71d58b3a61415a
Slava Chalnev <slava.chalnev@gmail.com>
2023-06-30 16:25:10 +1000

    Run multiple experiments

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 # %%
 import math
+import numpy as np
 import torch
+from tqdm import tqdm
 
 import matplotlib.pyplot as plt
 
@@ -30,16 +32,13 @@ def train(model, data, steps=50000, peak_lr=0.001, warmup_steps=2500):
     decay_func = lambda x: 0.5 * (1 + math.cos(math.pi * (x - warmup_steps) / (steps - warmup_steps)))
     lr_scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda x: warmup_func(x) if x <= warmup_steps else decay_func(x))
 
-    for step in range(steps):
+    for step in tqdm(range(steps)):
         optimizer.zero_grad()
         x_hat = model(data)
         l = loss(data, x_hat)
         l.backward()
         optimizer.step()
         lr_scheduler.step()
-        if step % 100 == 0:
-            print(step, l.item())
-            print(optimizer.param_groups[0]['lr'])
         
     T = data.shape[1]
     torch.save(model.state_dict(), f'model_T{T}.pt')
@@ -54,25 +53,30 @@ def train(model, data, steps=50000, peak_lr=0.001, warmup_steps=2500):
     
 # %%
 
-model = Model()
-data = get_data(T=10)
-train(model, data)#, steps=100000)
+# model = Model()
+# data = get_data(T=10)
+# train(model, data)#, steps=100000)
 
 # %%
 
-def plot_sample_h(data, model):
-    hs_x = []
-    hs_y = []
+def compute_fractional_dims(h):
     fractional_dims = []
 
-    with torch.no_grad():
-        h = model(data, return_h=True)
-    
     for h_i in h.T:
         numerator = torch.norm(h_i) ** 2
         h_i_hat = h_i / torch.norm(h_i)
         denominator = torch.sum((h_i_hat @ h) ** 2)
         fractional_dims.append(numerator / denominator)
+    
+    return fractional_dims
+
+
+def plot_sample_h(data, model):
+    hs_x = []
+    hs_y = []
+
+    with torch.no_grad():
+        h = model(data, return_h=True)
     
     hs_x = h[0].cpu().numpy()
     hs_y = h[1].cpu().numpy()
@@ -90,9 +94,9 @@ def plot_sample_h(data, model):
         plt.plot([0, x], [0, y], color='red')
     plt.show()
 
-    return fractional_dims
+    return compute_fractional_dims(h)
 
-plot_sample_h(data, model)
+# plot_sample_h(data, model)
 
 
 
@@ -100,7 +104,7 @@ plot_sample_h(data, model)
 
 def plot_feats(model):
     # plots columns of W
-    W = model.W.detach().cpu().numpy()
+    W = model.W.detach().cpu()
     # W is 2 x n
 
     plt.figure(figsize=(10, 10))
@@ -117,8 +121,50 @@ def plot_feats(model):
 
     plt.show()
 
-plot_feats(model)
+    return compute_fractional_dims(W)
+
+
+# plot_feats(model)
 
 # %%
 
+def run_experiment(T, device):
+    model = Model()
+    data = get_data(T)
+    train(model, data)
+    sample_dims = plot_sample_h(data, model)
+    feat_dims = plot_feats(model)
+
+    return sample_dims, feat_dims
+
+# run_experiment(5)
+
+# %%
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+ts = [3, 5, 6, 8, 10, 15, 30,]# 50, 100, 1000, 10000]
+
+sample_dims = []
+feat_dims = []
+
+for T in ts:
+    print('T', T)
+    sample_dim, feat_dim = run_experiment(T, device=device)
+    sample_dims.append(sample_dim)
+    feat_dims.append(feat_dim)
+
+# %%
+
+# plot sample and feat dims
+plt.figure(figsize=(10, 10))
+
+# For each T, plot all the corresponding sample_dims and feat_dims
+for T, s_dim, f_dim in zip(ts, sample_dims, feat_dims):
+    plt.scatter([T]*len(s_dim), s_dim, color='red')
+    plt.scatter([T]*len(f_dim), f_dim, color='blue')
+plt.show()
+
+
+# %%
 
