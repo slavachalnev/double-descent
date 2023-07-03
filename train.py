8698613e1f4e91010acc3cc7e08039437a0a2592
Slava Chalnev <slava.chalnev@gmail.com>
2023-07-03 11:06:56 +1000

    plot norms

--- train.py
+++ train.py
@@ -148,7 +148,7 @@ def run_experiment(T, device='cpu', dtype=torch.float32):
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 dtype = torch.float32
 
-ts = [3, 5, 6, 8, 10, 15, 30, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]
+ts = [3, 5, 6, 8, 10, 15, 30, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000]
 
 sample_dims = []
 feat_dims = []
